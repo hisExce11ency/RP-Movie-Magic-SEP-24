7277f504e0786128766736760186ca0755bda7e5
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2024-09-20 17:00:45 +0100

    Setup express
    import express, setup router to test and set up the host params.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import express from "express";
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send('Just works');
+});
+
+app.listen(5055, () => console.log('Server is listening on http://localhost:5055...'));
