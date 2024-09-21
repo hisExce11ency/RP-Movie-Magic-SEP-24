752dc1e34db118e9f3f5a51af14bc82361678b9d
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2024-09-21 17:34:19 +0100

    Create movies functionality

--- src/data/movieData.js
+++ src/data/movieData.js
@@ -1,21 +1,35 @@
 import fs from 'fs/promises';
 import path from 'path';
-import { log } from 'util';
+
+const dbPath = path.resolve('./src/db.json');
 
 async function getDb() {
-    const dbPath = path.resolve('./src/db.json');
     const jsonResult = await fs.readFile(dbPath, { encoding: 'utf-8' });
     const data = JSON.parse(jsonResult);
 
     return data;
 }
 
-async function getMovies() {
+function saveDb(data) {
+    return fs.writeFile(dbPath, JSON.stringify(data, {}, 2));
+}
+
+async function getAll() {
     const db = await getDb()
 
     return db.movies;
 }
 
+async function create(movieData) {
+    const db = await getDb();
+
+    db.movies.push(movieData);
+
+    return saveDb(db);
+
+}
+
 export default {
-    getMovies,
+    getAll,
+    create
 }
