a0227fd6413955f4e429da6bf20ade5d6684465a
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2024-09-28 14:38:46 +0100

    Extract handlebars and express configs
    Adding config folder to the frame and extract handlebars and express configs.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import express from "express";
-import handlebars from "express-handlebars";
 
 import routes from "./routes.js";
 
+import hendlebarsInit from "../config/handlevarsInit.js";
+import expressInit from "../config/expressInit.js";
+
 const app = express();
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-}));
-app.set('view engine', 'hbs');
-app.set('views', './src/views');
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+hendlebarsInit(app);
+expressInit
 
 app.use(routes);
 
