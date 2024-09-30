dbc10cf80e964f6d198e7dfb1bf1594528aaaee1
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2024-09-30 21:51:59 +0100

    Fix url

--- config/mongooseInit.js
+++ config/mongooseInit.js
@@ -1,13 +1,10 @@
 import { connect } from "mongoose";
 
-
-const dbUrl = "mongodb://localhost:27017/magic-movies";
-
 export default async function mongooseInit() {
     try {
-        const url = process.env.DB_URL || dbUrl;
-        await connect(url);
-        console.log('Successfully connected to DB!' + url);
+        const url = process.env.DB_URL || 'mongodb://localhost:27017';
+        await connect(url, { dbName: 'magic-movies' });
+        console.log('Successfully connected to DB!');
     } catch (err) {
         console.log("Can NOT connect to DB!" + err.message);
     }
